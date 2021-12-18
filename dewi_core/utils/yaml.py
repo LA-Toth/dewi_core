c2e37a466df2717e8b68d8873b8dcbf1f7149bc8
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2021-12-18 22:44:23 +0100

    yaml: add print_as_yaml to print to stdout, same as default save_to_yaml
    save_to_yaml's default output is the stdout, if it's None (default),
    or '-' as common in command-line args, like 'wget -O-'.
    print_as_yaml is added as a wrapper for convinience.

--- dewi_core/utils/yaml.py
+++ dewi_core/utils/yaml.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import typing
 
 import yaml
 
@@ -10,12 +11,16 @@ if os.environ.get('DEWI_YAML_WITH_ALIASES', '0') == '0':
     yaml.Dumper.ignore_aliases = lambda *args: True
 
 
-def save_to_yaml(cfg, output_file):
-    if output_file == '-':
+def save_to_yaml(cfg, output_file: typing.Optional[str] = None):
+    if not output_file or output_file == '-':
         yaml.dump(cfg, stream=sys.stdout, indent=4, default_flow_style=False)
     else:
         with open(output_file, 'wt', encoding='UTF-8') as f:
             yaml.dump(cfg, stream=f, indent=4, default_flow_style=False)
+
+
+def print_as_yaml(cfg):
+    save_to_yaml(cfg, '-')
 
 
 def load_yaml(filename: str):
